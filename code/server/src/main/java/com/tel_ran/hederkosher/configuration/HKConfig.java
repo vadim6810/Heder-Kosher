3ff39c91cb8fecb0754e8f2c528964d9f3b376e2
egorkozhevnikov <egor.kozhevnikov@gmail.com>
2016-08-29 11:12:08 +0300

    Add Lazy Fetch handling: * Add jackson-datatype-hibernate5 to pom.xml * Add jackson-datatype-hibernate5 to configuration class

--- code/server/src/main/java/com/tel_ran/hederkosher/configuration/HKConfig.java
+++ code/server/src/main/java/com/tel_ran/hederkosher/configuration/HKConfig.java
@@ -1,16 +1,23 @@
 package com.tel_ran.hederkosher.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @EnableWebMvc
 @ComponentScan(basePackages = "com.tel_ran.hederkosher.*")
-public class HKConfig {
+public class HKConfig{
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) {
@@ -36,5 +43,35 @@ public class HKConfig {
     class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
 
     }
+
+    @Configuration
+    class AppConfig extends WebMvcConfigurerAdapter {
+
+        /* Here we register the Hibernate5Module into an ObjectMapper,
+         * then set this custom-configured ObjectMapper
+         * to the MessageConverter and return it to be added to the HttpMessageConverters
+         * of our application
+         */
+        public MappingJackson2HttpMessageConverter jacksonMessageConverter(){
+            MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+
+            ObjectMapper mapper = new ObjectMapper();
+            //Registering Hibernate5Module to support lazy objects
+            mapper.registerModule(new Hibernate5Module());
+
+            messageConverter.setObjectMapper(mapper);
+            return messageConverter;
+
+        }
+
+        @Override
+        public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+            //Here we add our custom-configured HttpMessageConverter
+            converters.add(jacksonMessageConverter());
+            super.configureMessageConverters(converters);
+        }
+
+    }
+
 
 }
